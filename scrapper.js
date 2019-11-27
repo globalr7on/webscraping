6454636873321db48e2c90c650a2205621bd2bba
Victor <vramh.developer@gmail.com>
2019-11-27 08:37:55 -0400

    se muestra titulo y los tres tipos de precios

--- scrapper.js
+++ scrapper.js
@@ -13,64 +13,21 @@ function searchProduct(searchTerm){
     .then(body => {
         const cells = [];
         const $ = cheerio.load(body); 
-        $('.product-tile').each(function(i,element){
+        $('.name-product-plp').each(function(i,element){
             const $element =$(element);
-            const $price = $element.find('p.price-cencosud');
+            const $price = $('.item-price');
             const cell = {
-             
-                title: $element.text(),
-                price: $price.text()
+                title: $element.text().replace(/[\n\r]/g,' '),
+                price: $price.text().replace(/[\n\r]/g,' '),
             };
             //console.log($element.text());
             cells.push(cell);
         })
       return cells;
     });
-        
-}
+};         
 module.exports = {
     searchProduct
 };
 
-
-
-
-/*
-async function init(){
-    const $ = await request({
-        uri: 'https://www.paris.cl',
-        transform: body => cheerio.load(body)
-    });
-    writeStream1.write('Producto|Precio\n')
-    const productName = $('h6').each((i,el) => {
-        console.log(i, $(el).text())
-    });
-    const productPrice= $('.prices p.price-cencosud').each((i,el) => {
-        console.log(i, $(el).text())
-    })
-    writeStream1.write(`${productName}|${productPrice}\n`)
-}
-
-init(); 
-
-async function smarphone(){
-    const $ = await request({
-        uri: 'https://www.paris.cl/tecnologia/celulares/',
-        transform: body => cheerio.load(body)
-    });
-    writeStream2.write('Producto|Precio\n')
-    $('.js-search-result-wrapper').each((i,el) => {
-
-
-
-        const productName = $(el).find('.name-product-plp').text();
-        const productPrice = $(el).find('.item-price').text();
-        writeStream2.write(`${productName}|${productPrice}\n`)
-        console.log( 'producto:'+productName, 'precio:'+productPrice,);
-    })
-
-}
-smarphone(); 
-
-*/
 
